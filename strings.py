605d2077bedf211b2c305614545013c877767ed0
pennywise97 <aishwaryasolanki1997@gmail.com>
2019-10-03 21:37:36 +0530

    string ooperations in python

---
+++ strings.py
@@ -0,0 +1,8 @@
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+
+print('Hello ' + first_name + ' ' + last_name)
+print('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
+
+print('Hello ' + first_name.upper() + ' ' + last_name.upper())
+print('Hello ' + first_name.lower() + ' ' + last_name.lower())
