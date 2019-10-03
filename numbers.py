a114341d5f5a6b6fdf60ed6052e4943be2636041
pennywise97 <aishwaryasolanki1997@gmail.com>
2019-10-04 00:32:20 +0530

    numbers in python

---
+++ numbers.py
@@ -0,0 +1,19 @@
+pi = 3.14
+print('Value of pi is ' + str(pi))
+
+first_num = 2
+second_num = 6
+print('2 + 6 = ' + str(first_num) + str(second_num))
+print('2 ^ 6 = ' + str(first_num ** second_num))
+
+days_in_july = 31
+print(str(days_in_july) + ' days in july')
+
+first_num = 5
+second_num = 6
+print('5 + 6 = ' + str(first_num + second_num))
+
+first_num = input('Enter first number: ')
+second_num = input('Enter second number: ')
+print('Addition is ' + str(int(first_num) + int(second_num)))
+print('Float addition is ' + str(float(first_num) + float(second_num)))
