a357f17d6f14b73dff11f12a9bd5592abaf7ce56
pennywise97 <aishwaryasolanki1997@gmail.com>
2019-11-02 22:55:23 +0530

    modules in python

---
+++ helper.py
@@ -0,0 +1,5 @@
+
+def display_warning(message, is_warning = False):
+    if is_warning:
+        print(message)
+    print(message)
---
+++ use_helpers.py
@@ -0,0 +1,4 @@
+from helper import display_warning
+
+helper.display_warning('This Is A Warning')
+helper.display_warning('Warning', True)
